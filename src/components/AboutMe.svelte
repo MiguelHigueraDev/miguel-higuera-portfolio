f0524cb5d7dde22af28472c6857d74e96bd889e7
Miguel Higuera <miguelhiguera.dev@gmail.com>
2023-06-10 22:02:28 -0400

    Fixed about me height

--- src/components/AboutMe.svelte
+++ src/components/AboutMe.svelte
@@ -2,9 +2,9 @@
     import { t, locale, locales } from "../store";
     import DeveloperIcon from "./icons/DeveloperIcon.svelte";
 </script>
-<section id="about-me" class="relative w-full h-[1000px] md:h-screen mx-auto">
-    <div class="absolute inset-0 max-w-7xl mx-auto flex flex-col justify-start md:justify-center items-center">
-        <div class="block w-full h-[150px] md:h-[150px] xl:hidden"></div>
+<section id="about-me">
+    <div class="max-w-7xl mx-auto flex flex-col justify-start md:justify-center items-center">
+        <div class="block w-full h-[150px] md:h-[150px] lg:h-[120px] xl:h-[150px]"></div>
         <h1 class="text-green-400 text-5xl font-bold">{$t('about_me')}</h1>
         <h2 class="p-4 mt-5 md:mt-10 text-2xl text-white"> {$t('about_me_1')}</h2>
         <h2 class="p-4 mt-5 text-2xl text-white">{$t('about_me_2')}</h2>
@@ -12,7 +12,6 @@
             <a href="/technologies" class="text-center text-white tracking-wide text-xl md:text-2xl lg:text-2xl mb-10 font-semibold bg-green-500 hover:bg-green-300 hover:text-black p-6 rounded-lg shadow-xl">{$t('technologies')}</a>
             <a href="/contact" class="text-center text-white tracking-wide text-xl md:text-2xl lg:text-2xl mb-10 font-semibold bg-green-500 hover:bg-green-300 hover:text-black p-6 rounded-lg shadow-xl">{$t('contact')}</a>
         </section>
-        
-        <DeveloperIcon />
+        <DeveloperIcon width={600} height={600}/>
     </div>
 </section>
