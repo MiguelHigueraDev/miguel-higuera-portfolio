e6bbc7a8611f93cf8aa0a94b8392ca206272fe78
Miguel Higuera <miguelhiguera.dev@gmail.com>
2023-06-22 15:15:33 -0400

    Changed tech stack link to projects.

--- src/components/AboutMe.svelte
+++ src/components/AboutMe.svelte
@@ -9,7 +9,7 @@
         <h2 class="p-4 mt-5 md:mt-10 text-2xl text-white"> {$t('about_me_1')}</h2>
         <h2 class="p-4 mt-5 text-2xl text-white">{$t('about_me_2')}</h2>
         <section id="nav-buttons" class="flex flex-col md:flex-row md:gap-7 mt-10">
-            <a href="/technologies" class="text-center text-white tracking-wide text-xl md:text-2xl lg:text-2xl mb-10 font-semibold bg-green-500 hover:bg-green-300 hover:text-black p-6 rounded-lg shadow-xl">{$t('technologies')}</a>
+            <a href="/projects" class="text-center text-white tracking-wide text-xl md:text-2xl lg:text-2xl mb-10 font-semibold bg-green-500 hover:bg-green-300 hover:text-black p-6 rounded-lg shadow-xl">{$t('projects')}</a>
             <a href="/contact" class="text-center text-white tracking-wide text-xl md:text-2xl lg:text-2xl mb-10 font-semibold bg-green-500 hover:bg-green-300 hover:text-black p-6 rounded-lg shadow-xl">{$t('contact')}</a>
         </section>
         <DeveloperIcon width={600} height={600}/>
